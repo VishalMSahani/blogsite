6c6e32714d4114b4cbb2e568f1fde112f62e8858
Vishal Sahani <88318424+VishalMSahani@users.noreply.github.com>
2024-03-18 00:16:09 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='p-4  bg-white flex justify-center mb-4 w-full h-auto border-b-2 shadow-md'>
       <header className='text-3xl font-bold uppercase'>
-        Tach Blogs <span className=' lowercase  text-sm font-medium text-blue-600'>by</span> 
+        Tech Blogs <span className=' lowercase  text-sm font-medium text-blue-600'>by</span> 
         <span className='text-sm font-bold text-blue-600 '> Vishal Sahani</span> 
       </header>
     </div>
